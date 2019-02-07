a1a2f9a10ed1cd17f704781552e91cf62f4f8448
Agustin <agustin@edsurge.com>
2019-02-07 13:24:44 -0800

    refactor: and stlye: prefer using stateless functional components vs component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,26 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import BooksList from './BooksList'
 
-class BookShelf extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    title: PropTypes.string.isRequired,
-    updateBookShelf: PropTypes.func.isRequired
-  }
+function BookShelf(props) {
+  const { books, title, updateBookShelf } = props
+  return  <div className="bookshelf">
+            <h2 className="bookshelf-title">{title}</h2>
+            <div className="bookshelf-books">
+              <BooksList books={books} updateBookShelf={updateBookShelf} />
+            </div>
+          </div>
+}
 
-  render() {
-    const { books, title, updateBookShelf } = this.props
-
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{title}</h2>
-        <div className="bookshelf-books">
-          <BooksList books={books} updateBookShelf={updateBookShelf} />
-        </div>
-      </div>
-    )
-  }
+BookShelf.propTypes = {
+  books: PropTypes.array.isRequired,
+  title: PropTypes.string.isRequired,
+  updateBookShelf: PropTypes.func.isRequired
 }
 
 export default BookShelf
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,45 +1,38 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import BookShelf from './BookShelf'
 
-class ListBooks extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    updateBookShelf: PropTypes.func.isRequired
-  }
+function ListBooks(props) {
+  const { books, updateBookShelf } = props
+  return  <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
 
-  render() {
-    const { books, updateBookShelf } = this.props
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
+            <div className="list-books-content">
+              <BookShelf
+                books={books.filter(book => book.shelf === "currentlyReading")}
+                title="Currently Reading"
+                updateBookShelf={updateBookShelf} />
+              <BookShelf
+                books={books.filter(book => book.shelf === "wantToRead")}
+                title="Want to Read"
+                updateBookShelf={updateBookShelf} />
+              <BookShelf
+                books={books.filter(book => book.shelf === "read")}
+                title="Read"
+                updateBookShelf={updateBookShelf} />
+            </div>
 
-        <div className="list-books-content">
-          <div>
-            <BookShelf
-              books={books.filter(book => book.shelf === "currentlyReading")}
-              title="Currently Reading"
-              updateBookShelf={updateBookShelf} />
-            <BookShelf
-              books={books.filter(book => book.shelf === "wantToRead")}
-              title="Want to Read"
-              updateBookShelf={updateBookShelf} />
-            <BookShelf
-              books={books.filter(book => book.shelf === "read")}
-              title="Read"
-              updateBookShelf={updateBookShelf} />
+            <Link to="/search" className="open-search">
+              <button>Add a book</button>
+            </Link>
           </div>
-        </div>
-
-        <Link to="/search" className="open-search">
-          <button>Add a book</button>
-        </Link>
-      </div>
-    )
-  }
 }
 
+ListBooks.propTypes = {
+  books: PropTypes.array.isRequired,
+  updateBookShelf: PropTypes.func.isRequired
+}
 export default ListBooks
