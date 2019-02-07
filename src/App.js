f770247d0aee499a0a7e8de9f35a48c3a665e6fb
Agustin <agustin@edsurge.com>
2019-02-06 22:05:24 -0800

    feat: Now adding the routes to load the right components per url

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchBooks from './SearchBooks'
@@ -6,13 +7,6 @@ import ListBooks from './ListBooks'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: true,
     books: []
   }
   componentDidMount() {
@@ -26,11 +20,16 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBooks books={this.state.books} />
-        ) : (
-          <ListBooks books={this.state.books} />
-        )}
+        <Route exact path='/' render={() => (
+          <ListBooks
+            books={this.state.books}
+          />
+        )} />
+        <Route path='/search' render={() => (
+          <SearchBooks
+            books={this.state.books}
+          />
+        )} />
       </div>
     )
   }
